47a2ab5a718491e379d913f6c1ffc4021415a11f
Kuikka <ville_holopainen@msn.com>
2020-08-19 15:46:26 +0300

    update

---
+++ bmiCalculator.ts
@@ -0,0 +1,36 @@
+const calculateBmi = (args: Array<string>): string => {
+  const height = Number(args[2])
+  const weight = Number(args[3])
+
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  const BMI: number = weight / ((height / 100)**2);
+
+  switch(true) {
+    case isNaN(BMI):
+      throw new Error('Inputs must be numbers');
+    case BMI < 15:
+      return (`BMI is ${BMI} - Very severely underweight`);
+    case BMI < 16:
+      return (`BMI is ${BMI} - Severely underweight`);
+    case BMI < 18.5:
+      return (`BMI is ${BMI} - Underweight`);
+    case BMI < 25:
+      return (`BMI is ${BMI} - Normal (healthy weight)`);
+    case BMI < 30:
+      return (`BMI is ${BMI} - Overweight`);
+    case BMI < 35:
+      return (`BMI is ${BMI} - Obese Class I (Moderately obese)`);
+    case BMI < 40:
+      return (`BMI is ${BMI} - Obese Class II (Severely obese)`);
+    default:
+        return (`BMI is ${BMI} - Obese Class III (Very severely obese)`);  
+  }
+}
+
+try {
+  console.log(calculateBmi(process.argv))
+} catch (e) {
+  console.log('Something went wrong, error message: ', e.message);
+}
---
+++ exerciseCalculator.ts
@@ -0,0 +1,63 @@
+interface Atributes {
+  days: number,
+  trainingDays: number,
+  targetValue: number,
+  total: number,
+  averagetime: number,
+  targetReached: boolean,
+  rating: number,
+  text: string
+}
+
+const calculateExercises = (argv: Array<string>): Atributes => {
+  
+  if (argv.length < 2) throw new Error('Not enough arguments');
+
+  // week = poor name, means now just scanned input values
+  const week = argv.map(value => {
+    if (isNaN(Number(value))) {
+      throw new Error('Use only numbers');
+    } else  {
+      return Number(value)
+    }
+  })
+  
+  const days = week.length-1;
+  const trainingDays = week.filter(value => value > 0).length -1;
+  const targetValue = week[0];
+  const total = week.reduce((sum, day) => sum + day, 0)-targetValue;
+  const averagetime = total / trainingDays;
+  const targetReached = averagetime >= targetValue;
+  const result = (difference = targetValue - averagetime): any => {
+    switch(true) {
+      case difference <= 0:
+        return {value: 3, description: "Good week"};
+      case difference < 0.5:
+        return {value: 2, description: "not too bad but could be better"};
+      default:
+        return {value: 1, description: "Poor week"};  
+  }};
+  const rating = result().value;
+  const text = result().description;
+
+  const object = {
+    days: days,
+    trainingDays: trainingDays,
+    targetValue: targetValue,
+    total: total,
+    averagetime: Number(averagetime.toFixed(2)),
+    targetReached: targetReached,
+    rating: rating,
+    text: text
+  };
+  
+  return object;
+}
+
+const week = process.argv.slice(2); //copies arguments without two first
+try {
+  console.log(calculateExercises(week))
+} catch (e) {
+  console.log('Hay problemos!: ', e.message);
+}
+
